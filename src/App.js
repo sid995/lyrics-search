68c4a1c42dace92f7a6e5b8136e16c03fc6e5fdc
Siddharth Kundu <sk31395@gmail.com>
2018-09-06 23:25:02 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Navbar from "./component/layout/Navbar";
+import Index from "./component/layout/Index";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <React.Fragment>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={Index} />
+            </Switch>
+          </div>
+        </React.Fragment>
+      </Router>
     );
   }
 }
